6e3f4a239b666ad8726eb53696a541fb654132bf
Marvin Dela Cruz <mdela022@ucr.edu>
2018-12-14 16:30:54 +0000

    Working

--- Phase3/code/java/run.sh
+++ Phase3/code/java/run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-DBNAME=$MDELA022_DB
-PORT=$7567
-USER=$MDELA022 
+DBNAME=$1
+PORT=$2
+USER=$3 
 
 # Example: source ./run.sh flightDB 5432 user
 java -cp lib/*:bin/ MechanicShop $DBNAME $PORT $USER
