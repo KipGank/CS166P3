8b4b13f717a5767c40e11827f940303af00c0799
Marvin Dela Cruz <mdela022@ucr.edu>
2018-12-14 16:23:33 +0000

    ok

--- Phase3/code/java/run.sh
+++ Phase3/code/java/run.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 DBNAME=$MDELA022_DB
-PORT=$8465
+PORT=$7567
 USER=$MDELA022 
 
 # Example: source ./run.sh flightDB 5432 user
-cat <(echo 'CREATE SEQUENCE part_number_seq  START WITH 30000;')|psql -h 127.0.0.1 -p $PGPORT $USER"_DB"
 java -cp lib/*:bin/ MechanicShop $DBNAME $PORT $USER
