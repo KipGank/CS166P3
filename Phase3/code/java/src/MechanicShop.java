6f073f5bdee13a53bdb0cb488d6f36cc0009525e
Marvin Dela Cruz <mdela022@ucr.edu>
2018-12-15 00:30:53 +0000

    fixed

--- Phase3/code/java/src/MechanicShop.java
+++ Phase3/code/java/src/MechanicShop.java
@@ -60,14 +60,14 @@ public class MechanicShop{
 	
 	final static String DATE_FORMAT = "dd-MM-yyyy";
 
-	public static boolean isDateValid(String date) 
+	public static boolean checkDate(String date) 
 	{
         try {
             DateFormat df = new SimpleDateFormat(DATE_FORMAT);
             df.setLenient(false);
             df.parse(date);
             return true;
-        } catch (ParseException e) {
+        } catch (Exception e) {
             return false;
         }
 	}
